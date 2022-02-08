573c187ba3cf5e916e88099a3021f4f19c7c4bd9
manismk <manismkmech@gmail.com>
2022-02-08 23:15:20 +0530

    till 2/8

---
+++ week-4/script.js
@@ -0,0 +1,134 @@
+function strName(name, func) {
+  func(name.length);
+}
+
+strName("Manikandan", (length) => {
+  console.log(`OMG my name is ${length} long`);
+});
+
+function willThanosKillMe(name, success, fail) {
+  if (name.length % 2 === 0) {
+    success();
+  } else {
+    fail();
+  }
+}
+
+willThanosKillMe(
+  "Manikandan",
+  () => {
+    console.log("yes I am Alive");
+  },
+  () => {
+    console.log(`Give my headphones to sister`);
+  }
+);
+
+function delayMessage(message, delay) {
+  setTimeout(() => {
+    console.log(message);
+  }, delay);
+}
+
+delayMessage("Tanay is more energetic today", 3000);
+
+function fakeFetch(msg, shouldReject) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldReject) {
+        reject(`error from server: ${msg}`);
+      }
+      resolve(`from server: ${msg}`);
+    }, 3000);
+  });
+}
+
+fakeFetch("mk")
+  .then((sucess) => {
+    console.log(sucess);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+fakeFetch("Hello")
+  .then((str) => str.length)
+  .then((len) => console.log(len))
+  .catch((e) => console.log(e));
+
+const getServerResponseLength = (str) =>
+  fakeFetch(str).then((res) => res.length);
+
+getServerResponseLength("helloMk").then((res) => console.log(res));
+
+const syncCall = (msg1, msg2) => {
+  fakeFetch(msg1).then((res) => {
+    console.log(res);
+    fakeFetch(msg2).then((res2) => console.log(res2));
+  });
+};
+
+const asyncCall = async () => {
+  try {
+    const data = await fakeFetch("Tanay");
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const asyncWaterFall = async (msg1, msg2) => {
+  try {
+    const data = await fakeFetch(msg1);
+    console.log(data);
+    const data2 = await fakeFetch(msg2);
+    console.log(data2);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+syncCall("tanay", "pratap");
+
+asyncCall();
+
+asyncWaterFall("tanay", "pratap");
+
+// Home works
+
+// set interval
+
+const messagedelay = (message, delay) => {
+  setInterval(() => {
+    console.log(message);
+  }, delay);
+};
+
+// messagedelay("Mani", 0);
+
+const countDown = (number) => {
+  console.log(number);
+  const clearId = setInterval(() => {
+    number--;
+    if (number === 0) {
+      console.log("Bang Bang");
+      clearInterval(clearId);
+    } else {
+      console.log(number);
+    }
+  }, 1000);
+};
+
+countDown(5);
+
+// React code https://codesandbox.io/s/week-4-tany-hw-nv5yg?file=/src/App.js
+
+//get response length async
+
+const getServerResponseLengthAsync = async (str) => {
+  const res = await fakeFetch(str);
+  return res.length;
+};
+getServerResponseLengthAsync("hellomk").then((res) =>
+  console.log(`132 ${res}`)
+);
---
+++ week-5/script.js
@@ -0,0 +1,262 @@
+const tanay = { mother: "mother name", age: 30 };
+const tanvi = { ...tanay, age: tanay.age - 4 };
+
+console.log(tanay, tanvi);
+
+const colors1 = ["red", "orange", "pink", "black", "white"];
+const colors2 = [...colors1, "blue", "yellow"];
+
+const birthday = (person) => ({ ...person, age: person.age + 1 });
+console.log(birthday({ name: "tanay", age: 30 }));
+
+const numArray = [2, 5, 8, 9, 10, 15, 25];
+const lessThan10 = (num) => num < 10;
+console.log(numArray.filter(lessThan10));
+
+const numIncrement2 = numArray.map((x) => x + 2);
+console.log(numIncrement2);
+
+const numberObject2 = numArray.map((num) => ({ number: num }));
+console.log(numberObject2);
+
+const numberObject = numArray.map((num, index) => ({
+  Number: num,
+  index: index,
+}));
+
+console.log(numberObject);
+
+const reduceFunc = numArray.reduce((acc, curr) => acc + curr);
+
+console.log(reduceFunc);
+
+const sumOFfNumLessThan10 = numArray.reduce((acc, curr) => {
+  if (curr < 10) return acc + curr;
+  else return acc;
+}, 0);
+
+console.log(sumOFfNumLessThan10);
+
+const givenArray = [1, 3, 5, 2, 22, 11, 9];
+const sumOFOddNumber = givenArray.reduce((acc, curr) => {
+  if (curr % 2 != 0) return acc + curr;
+  else return acc;
+}, 0);
+
+console.log(sumOFOddNumber);
+
+const sumOFOddNumberAndEven = givenArray.reduce(
+  (acc, curr) => {
+    if (curr % 2 != 0) return { ...acc, oddSum: acc.oddSum + curr };
+    else return { ...acc, evenSum: acc.evenSum + curr };
+  },
+  { oddSum: 0, evenSum: 0 }
+);
+console.log(sumOFOddNumberAndEven);
+
+const nameWithStatement = (userName) => (statement) =>
+  `${userName} say's ${statement}`;
+
+console.log(nameWithStatement("Tanay")("Hello"));
+
+const nameFunction = (statement) => `Tanay says ${statement}`;
+const idFunction = (statement) => `ID:123 says ${statement}`;
+
+const composFunction = (statement) =>
+  `${nameFunction(statement)} ${idFunction(statement)}`;
+
+console.log(composFunction("mk"));
+
+// Home work questions
+
+console.log(numArray);
+
+const ownReduce = (arr, intial) => {
+  for (item in arr) {
+    if (arr[item] % 2 != 0) intial = intial + arr[item];
+    else intial;
+  }
+  return intial;
+};
+
+console.log(ownReduce(numArray, 0));
+
+// Akansha functional programming session
+
+const arr2 = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
+
+const addOneForEven = arr2.map((ele) => (ele % 2 === 0 ? ele + 1 : ele));
+
+console.log(addOneForEven);
+
+const arr3 = [
+  {
+    name: "Jay",
+    age: 60,
+  },
+  {
+    name: "Gloria",
+    age: 36,
+  },
+  {
+    name: "Manny",
+    age: 16,
+  },
+  {
+    name: "Joe",
+    age: 9,
+  },
+];
+
+const allAge = arr3.reduce((acc, curr) => acc + curr.age, 0);
+
+console.log(allAge);
+
+const family = [
+  {
+    name: "Tanay",
+    haveCycle: true,
+  },
+  {
+    name: "Akanksha",
+    haveCycle: false,
+  },
+  {
+    name: "Tanvi",
+    haveCycle: true,
+  },
+  {
+    name: "Kanak",
+    haveCycle: false,
+  },
+];
+
+const haveCyclePeople = family
+  .filter((el) => el.haveCycle)
+  .map((el) => el.name);
+
+console.log(haveCyclePeople);
+
+const haveCyclePeopleReduce = family.reduce(
+  (acc, curr) => (curr.haveCycle ? [...acc, curr.name] : acc),
+  []
+);
+
+console.log(haveCyclePeopleReduce);
+
+const arr4 = ["You", "all", "are", "rockstars"];
+
+const arrayWithObject = arr4.reduce(
+  (acc, curr, currIndex) => ({ ...acc, [currIndex]: curr }),
+  {}
+);
+
+console.log(arrayWithObject);
+
+const arr = ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"];
+
+// Your output should be:
+// Violet-Indigo-Blue-Green-Yellow-Orange-Red
+
+const vibgyorConcat = arr.join("-");
+
+console.log(vibgyorConcat);
+
+const input = [
+  ["a", "b", "c"],
+  ["c", "d", "e"],
+  ["e", "d", "f"],
+];
+
+console.log(input.reduce((acc, curr) => [...acc, ...curr], []));
+
+const arr5 = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
+
+const numberLessThan8andEven = arr5.reduce(
+  (acc, curr) => (curr <= 8 && curr % 2 === 0 ? [...acc, curr] : acc),
+  []
+);
+
+console.log(numberLessThan8andEven);
+
+const arr6 = ["eat", "sleep", "repeat", "code"];
+
+const wordsMoreThan5Chars = arr6.filter((word) => word.length > 5);
+
+console.log(wordsMoreThan5Chars);
+
+const arr7 = [1, 2, 3, 58, 5, 6, 62, 8, 70];
+
+const sumOfNumGreaterThan50 = arr7.reduce(
+  (acc, curr) => (curr > 50 ? acc + curr : acc),
+  0
+);
+
+console.log(sumOfNumGreaterThan50);
+
+const arr8 = [1, 2, 3, 7, 5, 6, 8, 9];
+
+const productOfEvenNumbers = arr8.reduce(
+  (acc, curr) => (curr % 2 === 0 ? acc * curr : acc),
+  1
+);
+
+console.log(productOfEvenNumbers);
+
+const arr9 = [
+  {
+    name: "Apple",
+  },
+  {
+    name: "Mango",
+  },
+  {
+    name: "Potato",
+  },
+  {
+    name: "Guava",
+  },
+  {
+    name: "Capsicum",
+  },
+];
+
+const addType = arr9.map((item) =>
+  item.name.length > 5
+    ? { ...item, type: "vegetable" }
+    : { ...item, type: "fruit" }
+);
+
+console.log(addType);
+
+const inventory = [
+  { name: "fans", quantity: 3 },
+  { name: "chimneys", quantity: 0 },
+  { name: "bulbs", quantity: 5 },
+  { name: "stove", quantity: 1 },
+];
+
+const inventoryQuantityLessThan2 = inventory.filter(
+  (item) => item.quantity < 2
+);
+
+console.log(inventoryQuantityLessThan2);
+
+const totalQuantityOfItem = inventory.reduce(
+  (acc, curr) => acc + curr.quantity,
+  0
+);
+
+console.log(totalQuantityOfItem);
+
+const inventoryWhoseQuantityZero = inventory.filter(
+  (item) => item.quantity === 0
+);
+
+console.log(inventoryWhoseQuantityZero);
+
+const num = 24345687;
+
+const seperateNumberByHyphen = (arr) => arr.map((e) => console.log(e));
+
+// seperateNumberByHyphen(...num);
