620dd1efb00509eb6c0610cbef182ba9fd8c8035
manismk <manismkmech@gmail.com>
2022-02-28 10:45:48 +0530

    Exercise untill 28/2

---
+++ week-6,7/index.js
@@ -0,0 +1,17 @@
+//   # Initial State: { isPasswordMatched: true }
+
+// # Initial View
+// - isPasswordMatched? if yes, then don't show error, else show error.
+// - View: error not shown
+
+// #  Event
+// - Read Input.
+// - Use that Input to check password.
+
+// # new state
+// - if password === confirmPassword then set state to { isPasswordMatched: true }
+
+// # new view
+// - isAgeLessThan? if yes, then show error, else keep error hidden.
+// - View: error Shown.
+//
